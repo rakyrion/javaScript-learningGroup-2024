0dc51449c4b56070ae2a34cf6308e08e2b74fab4
Adrian Ruiz <adrian.ruiz@singulardevs.com>
2023-12-29 13:59:19 +0100

    add includes / indexOf / join base exercise

---
+++ staff/adrian-ruiz/arrays/includes.js
@@ -0,0 +1,29 @@
+const coches = ['BMW', 'Ford', 'Audi', 'Seat']
+
+function includesFunction() {
+	// TODO: IMPLEMENTAR PARÁMETROS Y FUNCIÓN
+
+}
+
+console.log(includesFunction(coches, 'Audi'))
+// true
+
+console.log(includesFunction(coches, 'audi'))
+// false
+
+console.log(includesFunction(coches, 'Ford', 1))
+// true
+
+console.log(includesFunction(coches, 'Ford', 3))
+// false
+
+
+// A PARTIR DE AQUÍ LOS HAREMOS JUNTOS
+// console.log(includesFunction(coches, 'Ford', -2))
+// false
+
+// console.log(includesFunction(coches, 'Ford', -3))
+// true
+
+// console.log(includesFunction(coches, 'Ford', 8))
+// false
---
+++ staff/adrian-ruiz/arrays/join.js
@@ -0,0 +1,18 @@
+
+
+const ciudades = ['Madrid', 'Barcelona', 'Castellón', 'Alicante']
+
+
+function joinFunction () {
+	// TODO: IMPLEMENTAR PARÁMETROS Y FUNCIÓN
+}
+
+
+console.log(joinFunction(ciudades, ','))
+// "Madrid,Barcelona,Castellón,Alicante"
+
+console.log(joinFunction(ciudades, '/'))
+// "Madrid/Barcelona/Castellón/Alicante"
+
+console.log(joinFunction(ciudades, ' '))
+// "Madrid Barcelona Castellón Alicante"
